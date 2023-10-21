b92cf0624bc5722a14ba0bfed4d751365f48b6ad
Giorgi Chagelishvili <chagx116@gmail.com>
2023-10-21 12:25:15 +0400

    create button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <button :id="id">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: ['id', 'text']
+}
+</script>
+
+<style scoped>
+button {
+  font-size: 16px;
+  padding-inline: 10px;
+  padding-block: 5px;
+
+  background: none;
+  border-width: 1px;
+
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+button:hover {
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.4);
+  -webkit-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.4);
+  -moz-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.4);
+}
+</style>
--- src/components/PetListHeader.vue
+++ src/components/PetListHeader.vue
@@ -1,9 +1,24 @@
 <template>
   <h1>Pet List</h1>
+
+  <div>
+    <ButtonComponent text="Add" />
+    <ButtonComponent text="Delete" />
+  </div>
 </template>
 
 <script>
+import ButtonComponent from './ButtonComponent.vue'
 export default {
-  name: 'PetListHeader'
+  name: 'PetListHeader',
+  components: { ButtonComponent }
 }
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+</style>
